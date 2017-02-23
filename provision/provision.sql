ef4115bfe1b83d89ac818f41d196af3e7cee01ec
Patrick Spencer <pkspenc@gmail.com>
2017-02-22 21:21:21 -0500

    Add sql provision file

---
+++ provision/provision.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE okapi_db;
+CREATE USER okapi WITH PASSWORD 'password';
+ALTER ROLE okapi SET client_encoding TO 'utf8';
+ALTER ROLE okapi SET default_transaction_isolation TO 'read committed';
+ALTER ROLE okapi SET timezone TO 'UTC';
+ALTER ROLE "okapi" WITH LOGIN;
+GRANT ALL PRIVILEGES ON DATABASE okapi_db TO okapi;
