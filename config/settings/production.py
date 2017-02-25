167fe3736b14af328777328fe6556f3af57beff7
Patrick Spencer <pkspenc@gmail.com>
2017-02-25 15:02:50 -0500

    Fix import of common files in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,5 @@
 import os
+from .common import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -6,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '95m0ei^4n6q(+*ty-1@=x#^%epu#6g!9y2fog#j6vs&h$rq7-h'
 
-# I meant to leave this as True for production. I want to know what is 
+# I meant to leave this as True for production. I want to know what is
 # breaking on the server
 DEBUG = True
 
