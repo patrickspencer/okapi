d4f770cc7cf9f5e3de173913fe5a7f83d9ee9b64
Patrick Spencer <pkspenc@gmail.com>
2017-02-25 19:58:22 -0500

    Fix bug with static files
    * Adding a comma after first item in STATICFILES_DIRS fixed an error
      which said it couldn't find static files

--- config/settings/common.py
+++ config/settings/common.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(MAIN_APP_DIR, "static"))
+    os.path.abspath(os.path.join(MAIN_APP_DIR, "assets")),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
