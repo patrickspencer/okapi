c71898b2dec47c7bf00b4e6a14e55ae1d0014c46
Patrick Spencer <pkspenc@gmail.com>
2017-03-09 11:57:20 -0500

    Add models
    These models should match existing database schema created by ewalk
    sqlalchemy models

--- okapi/apps/ewalk/models.py
+++ okapi/apps/ewalk/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Symbol(models.Model):
+
+    name = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = 'symbols'
+
+
+class Quote(models.Model):
+    date = models.DateField()
+    open = models.FloatField()
+    low = models.FloatField()
+    high = models.FloatField()
+    close = models.FloatField()
+    adj_close = models.FloatField()
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    volume = models.IntegerField()
+
+    class Meta:
+        db_table = 'quotes'
