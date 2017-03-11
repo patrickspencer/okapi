37523918c7ce9f2e8fb7e89fe195ca14106f2bbd
Patrick Spencer <pkspenc@gmail.com>
2017-03-11 01:45:27 -0500

    Update home view

--- okapi/apps/ewalk/views.py
+++ okapi/apps/ewalk/views.py
@@ -1,6 +1,7 @@
 import datetime
+from ewalk.models import Quote
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    q = Quote.objects.all()
+    return render(request, 'home.html', {'quotes': q})
