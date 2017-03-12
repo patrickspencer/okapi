c61d6d6856a6fb289d7755598e534da24e2a69f1
Patrick Spencer <pkspenc@gmail.com>
2017-03-12 05:04:13 +0000

    Add Symbol test to index view

--- okapi/apps/ewalk/views.py
+++ okapi/apps/ewalk/views.py
@@ -1,7 +1,11 @@
 import datetime
-from ewalk.models import Quote
+from ewalk.models import Symbol, Quote
 from django.shortcuts import render
 
 def index(request):
     q = Quote.objects.all()
-    return render(request, 'home.html', {'quotes': q})
+    s = Symbol.objects.all()
+    return render(request, 'home.html', {
+        'quotes': q,
+        'symbols': s,
+        })
