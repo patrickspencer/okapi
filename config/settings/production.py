4d875b25169cbb593c958cf4d732e1bc716a69cf
Patrick Spencer <pkspenc@gmail.com>
2017-03-23 00:00:47 -0400

    First change to settings environment vars
    Pull Django settings vars from environment vars instead of from a config
    file.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,31 +1,24 @@
 import os
-import configparser
 from .common import *
-
-config = configparser.RawConfigParser()
-config.read('/etc/okapi_production_settings.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('secrets', 'SECRET_KEY')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# I meant to leave this as True for production. I want to know what is
-# breaking on the server
-DEBUG = config.get('general', 'DEBUG')
+DEBUG = False
 
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': config.get('database', 'DATABASE_NAME'),
-    'USER': config.get('database', 'DATABASE_USER'),
-    'PASSWORD': config.get('database', 'DATABASE_PASSWORD'),
-    'HOST': config.get('database', 'DATABASE_HOST'),
-    'PORT': config.get('database', 'DATABASE_PORT'),
+    'NAME': os.environ['DATABASE_NAME'],
+    'USER': os.environ['DATABASE_NAME'],
+    'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    'HOST': os.environ['DATABASE_HOST'],
+    'PORT': os.environ['DATABASE_PORT'],
 }
 }
 
-ALLOWED_HOSTS = [
-        config.get('general', 'ALLOWED_HOSTS')
-        ]
+ALLOWED_HOSTS = [host in os.environ['ALLOWED_HOSTS']]
+
