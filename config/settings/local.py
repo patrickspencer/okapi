77202576b1fb84127184c47aee8c2f33bdcc1a34
Patrick Spencer <pkspenc@gmail.com>
2017-03-23 00:31:02 -0400

    Add local settings file that uses environ vars

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,18 +1,24 @@
 import os
 from .common import *
 
-# this file is only for local development so we can keep the secret key here
-SECRET_KEY = '95m0ei^4n6q(+*ty-1@=x#^%epu#6g!9y2fog#j6vs&h$rq7-h'
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = True
 
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'okapi_db',
-    'USER': 'okapi',
-    'PASSWORD': 'password',
-    'HOST': 'localhost',
-    'PORT': '',
+    'NAME': os.environ['DATABASE_NAME'],
+    'USER': os.environ['DATABASE_NAME'],
+    'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    'HOST': os.environ['DATABASE_HOST'],
+    'PORT': os.environ['DATABASE_PORT'],
 }
 }
+
+ALLOWED_HOSTS = [host for host in os.environ['ALLOWED_HOSTS']]
+
