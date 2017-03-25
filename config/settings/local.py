428aa77d5c68ae790599b06d303a9637b6362e68
Patrick Spencer <pkspenc@gmail.com>
2017-03-25 16:29:13 -0400

    Update config files
    Fix environ calls bug: change os.environ['...'] to os.environ.get('...')

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,21 +4,22 @@ from .common import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# Do not use 789j3kyb+yvd$^dqt8lmd%-f$dxw^_rr@oc5!wn$wg2j(6&s!3 as the
+# secret key in production!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','789j3kyb+yvd$^dqt8lmd%-f$dxw^_rr@oc5!wn$wg2j(6&s!3')
 
 DEBUG = True
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': os.environ['DATABASE_NAME'],
-    'USER': os.environ['DATABASE_NAME'],
-    'PASSWORD': os.environ['DATABASE_PASSWORD'],
-    'HOST': os.environ['DATABASE_HOST'],
-    'PORT': os.environ['DATABASE_PORT'],
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_NAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
 }
 
-ALLOWED_HOSTS = [host for host in os.environ['ALLOWED_HOSTS']]
+ALLOWED_HOSTS = [host for host in os.environ.get('ALLOWED_HOSTS','localhost').split()]
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -5,20 +5,20 @@ from .common import *
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': os.environ['DATABASE_NAME'],
-    'USER': os.environ['DATABASE_NAME'],
-    'PASSWORD': os.environ['DATABASE_PASSWORD'],
-    'HOST': os.environ['DATABASE_HOST'],
-    'PORT': os.environ['DATABASE_PORT'],
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_NAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
 }
 
-ALLOWED_HOSTS = [host for host in os.environ['ALLOWED_HOSTS']]
+ALLOWED_HOSTS = [host for host in os.environ.get('ALLOWED_HOSTS','localhost').split()]
 
